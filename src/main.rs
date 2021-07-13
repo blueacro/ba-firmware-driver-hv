fdde5557349371d86df0aa7fc7d0a4b64bfcfaab
Yann Ramin <github@theatr.us>
2021-07-13 15:28:25 -0700

    Basic fade test of the TPS61196 variant

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,21 @@
 extern crate ba_driver_hv_bsp as hal;
 extern crate panic_rtt_target;
 
-use rtt_target::{rtt_init_print, rprintln};
+use core::f32;
+use num_traits::float::FloatCore;
+
+use atsamd_hal::gpio::{PinId, PinMode};
 use cortex_m::peripheral::NVIC;
 use cortex_m_rt::{exception, ExceptionFrame};
 use hal::clock::GenericClockController;
 use hal::delay::Delay;
 use hal::entry;
 use hal::pac::{interrupt, CorePeripherals, Peripherals};
-use hal::pwm::{Channel, TCC0Pinout, Tcc0Pwm};
 use hal::prelude::*;
+use hal::pwm::{Channel, TCC0Pinout, Tcc0Pwm};
 use hal::usb::UsbBus;
 use hal::watchdog::{Watchdog, WatchdogTimeout};
+use rtt_target::{rprintln, rtt_init_print};
 
 use usb_device::bus::UsbBusAllocator;
 use usb_device::prelude::*;
@@ -23,6 +27,22 @@ use usbd_serial::{SerialPort, USB_CLASS_CDC};
 static mut USB_ALLOCATOR: Option<UsbBusAllocator<UsbBus>> = None;
 static mut USB_BUS: Option<UsbDevice<UsbBus>> = None;
 static mut USB_SERIAL: Option<SerialPort<UsbBus>> = None;
+
+fn set_duty_cycle<A: PinId, B: PinMode>(pwm0: &mut Tcc0Pwm<A, B>, percent: f32) {
+    let max_duty = pwm0.get_max_duty();
+    let value = (max_duty as f32 * percent.clamp(0f32, 1f32)).ceil() as u32;
+    let channels = [
+        Channel::_0,
+        Channel::_1,
+        Channel::_2,
+        Channel::_3,
+        Channel::_4,
+        Channel::_5,
+    ];
+    for channel in channels {
+        pwm0.set_duty(channel, value);
+    }
+}
 
 #[entry]
 fn main() -> ! {
@@ -37,6 +57,11 @@ fn main() -> ! {
         &mut peripherals.NVMCTRL,
     );
 
+    // Configure the supply supervisor, disable BOD33
+    peripherals
+        .SUPC
+        .bod33
+        .write(|w| { w.action().bkup() });
 
     let mut delay = Delay::new(core.SYST, &mut clocks);
 
@@ -46,40 +71,26 @@ fn main() -> ! {
     let mut led_red = sets.led.led_red.into_open_drain_output(&mut sets.port);
     let mut led_blue = sets.led.led_blue.into_open_drain_output(&mut sets.port);
     let mut dim_en = sets.dim_en.into_push_pull_output(&mut sets.port);
-    let mut dim_pin = sets.dim.into_push_pull_output(&mut sets.port);
-    let mut iset_pin = sets.dim_iset.into_push_pull_output(&mut sets.port);
+    let fault = sets.fault;
 
-    dim_pin.set_low().unwrap();
     led_blue.set_high().unwrap();
-
-    iset_pin.set_low().unwrap();
     dim_en.set_low().unwrap();
-    rprintln!("iset/dim/dim_en pin set low");
-    
-    delay.delay_ms(1000u16);
-    
-    iset_pin.set_high().unwrap();
-    dim_en.set_high().unwrap();
-    rprintln!("iset/dim_en pin set high");
-    led_blue.set_low().unwrap();
+
     delay.delay_ms(400u16);
-    dim_pin.set_high().unwrap();
-    rprintln!("dim pin set high");
-    /*
+    led_blue.set_low().unwrap();
+    dim_en.set_high().unwrap();
+    delay.delay_ms(100u16);
+
     let gclk = clocks.gclk0();
     let mut pwm0 = Tcc0Pwm::new(
         &clocks.tcc0_tcc1(&gclk).unwrap(),
-        100.khz(),
+        20.khz(),
         peripherals.TCC0,
-        TCC0Pinout::Pb13(sets.dim_iset.into_function_g(&mut sets.port)),
+        TCC0Pinout::Pb15(sets.dim.into_function_g(&mut sets.port)),
         &mut peripherals.MCLK,
     );
-    let max_duty = pwm0.get_max_duty();
+    set_duty_cycle(&mut pwm0, 0.02f32);
 
-
-    pwm0.set_duty(Channel::_1, max_duty - 10);
-
-*/
     let bus_allocator = unsafe {
         USB_ALLOCATOR = Some(sets.usb.init(
             peripherals.USB,
@@ -111,18 +122,26 @@ fn main() -> ! {
         NVIC::unmask(interrupt::USB_TRCPT1);
     }
 
-        //let mut wdt = Watchdog::new(peripherals.WDT);
+    //let mut wdt = Watchdog::new(peripherals.WDT);
     //wdt.start(WatchdogTimeout::Cycles256 as u8);
 
     let mut count: u32 = 0;
     loop {
         delay.delay_ms(200u8);
         led.set_high().unwrap();
-        led_red.set_high().unwrap();
+        match fault.is_high() {
+            Ok(true) => {
+                led_red.set_high().unwrap();
+            }
+            _ => {
+                led_red.set_low().unwrap();
+            }
+        }
         delay.delay_ms(200u8);
+        set_duty_cycle(&mut pwm0, ((count + 30) % 200) as f32 / 200.0f32);
         led.set_low().unwrap();
         count += 1;
-    rprintln!("looped {}", count);
+        rprintln!("looped {}", count);
     }
 }
 
